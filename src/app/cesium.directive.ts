60731e112ceea77c00cd7d4ba4efe70e7b213849
Juan Carlos <juancarlos.rmr@gmail.com>
2018-08-18 14:05:34 +0200

    cesium app ejemplo

---
+++ src/app/cesium.directive.ts
@@ -0,0 +1,13 @@
+import { Directive, OnInit, ElementRef } from "@angular/core";
+
+@Directive({
+  selector: "[appCesium]"
+})
+export class CesiumDirective implements OnInit {
+  constructor(private el: ElementRef) {}
+
+  ngOnInit() {
+    // Put initialization code for the Cesium viewer here
+    const viewer = new Cesium.Viewer(this.el.nativeElement);
+  }
+}
